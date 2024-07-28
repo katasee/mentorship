0ad9d3791f7759c017c469c97daeaa2c47d2aa4a
katasee <kotalov.developer@gmail.com>
2024-07-28 15:24:34 +0200

    add new playground

---
+++ HackingWithSwift/week1/Multi-line strings .playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str1 = """
+This goes \
+over multiple \
+lines
+"""
+var str2 = """
+this text
+i write
+like a test
+"""
+
+var str3 = """
+i think \
+i need write \
+with slash
+"""
+
