6058d8b87bfd7510ad50575ece4e5d76c61895f0
katasee <kotalov.developer@gmail.com>
2024-08-07 22:20:56 +0200

    add Git ignor

---
+++ HackingWithSwift/week1/day_4/For loops/For loops.playground/Contents.swift
@@ -0,0 +1,26 @@
+let names = ["Anton", "Vlad", "Mykola", "Ivan"]
+
+for name in names {
+    print ("\(name) is Barcelona players")
+}
+
+
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+let cocunt = 1...3
+for _ in cocunt {
+    print("There's no place like home.")
+}
+let count = 1..<5
+
+for number in count {
+    print ("last number is \(number)")
+}
+
+for number in [2, 3, 5] {
+    print("\(number) is a prime number.")
+}
---
+++ HackingWithSwift/week1/day_4/While loops/While loops .playground/Contents.swift
@@ -0,0 +1,7 @@
+var  number = 1
+
+while number <= 20 {
+    print (number)
+    number += 1
+}
+print ("redy to fight")
