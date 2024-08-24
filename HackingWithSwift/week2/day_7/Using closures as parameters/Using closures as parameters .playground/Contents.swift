4070dc36125e809b1adcf9ed160d536ce8f5216f
katasee <kotalov.developer@gmail.com>
2024-08-24 23:21:18 +0200

    day_7 (#13)
    * day_7
    
    * Update

---
+++ HackingWithSwift/week2/day_7/Using closures as parameters/Using closures as parameters .playground/Contents.swift
@@ -0,0 +1,17 @@
+func cashOut(bank: (String) -> Void) {
+    print("My firm sent salary to my bank account.")
+    bank("mBank")
+    print("I comeback home")
+}
+cashOut { (place: String) in
+    print("I'm going to \(place) to cashout")
+}
+
+func buyProducts(app: (String) -> Void) {
+    print("I need to buy grocery very fast")
+          app("Lisek.app")
+          print("I take it in 10 minutes")
+}
+buyProducts { (application: String) in
+    print("I installed \(application) and order products in it" )
+}
