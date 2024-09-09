e3edbca92d61b524f1eda68b945cc0cf48e7a1c3
Oleksandr Zavazhenko <olza.developer@gmail.com>
2024-09-10 01:41:45 +0200

    Day 8

---
+++ HackingWithSwift/week2/day_8/Mutating methods/Mutating methods .playground/Contents.swift
@@ -0,0 +1,45 @@
+struct Apple{
+    var date: Int
+    
+    mutating func presentation (){
+        date = 8
+    }
+}
+var apple = Apple(date: 9)
+apple.presentation()
+print(apple)
+
+struct Telegram {
+    var update: String
+    
+    mutating func function () {
+        update = "telegram Premium"
+    }
+}
+var telegram = Telegram(update: "stars for donate")
+telegram.update
+print(telegram)
+
+struct Investition {
+    var stocks: String
+    var price: Int
+    
+    mutating func updatePrice() {
+        price = 200
+    }
+}
+var investition = Investition(stocks: "AAPL", price: 220)
+investition.price
+print(investition)
+
+struct BankAccount {
+    var balance : Int
+    mutating func payForFood(amout: Int){
+      balance = 1200
+        balance -= amout
+    }
+}
+var updateBankAccount = BankAccount(balance: 1000)
+updateBankAccount.balance
+print(updateBankAccount)
+
---
+++ HackingWithSwift/week2/day_8/Properties and methods of strings/Properties and methods of strings .playground/Contents.swift
@@ -0,0 +1,6 @@
+let string = "I belive, I can fly"
+print(string.count)
+print(string.hasPrefix("I"))
+print(string.uppercased())
+print(string.sorted())
+
