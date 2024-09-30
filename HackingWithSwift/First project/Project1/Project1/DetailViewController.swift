a992229585d26dd058f96bb206e28d523992e5bd
katasee <kotalov.developer@gmail.com>
2024-09-30 22:29:13 +0200

    First project UIKit (#20)
    * First project UIKit
    
    * Project 1 UIKit update
    
    * Project perfectly done
    
    * Fix project 1

---
+++ HackingWithSwift/First project/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Katasee on 28/09/2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var imageTitle: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = imageTitle
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+}
