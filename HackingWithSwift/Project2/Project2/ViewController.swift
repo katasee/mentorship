ab9f2237cb7a2378db72fc4db5bc9a4f6ba0bfe9
katasee <kotalov.developer@gmail.com>
2024-10-03 14:48:05 +0200

    Project 2

--- HackingWithSwift/Project2/Project2/ViewController.swift
+++ HackingWithSwift/Project2/Project2/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     
     var countries = [String]()
-    var score = 0
     var correctAnswer = 0
+    let scoreLable = UILabel()
     
+    var score = 0 {
+        didSet {
+            scoreLable.text = "Score: \(score)"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,33 +35,60 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
+        finishGame()
+        
     }
+    func finishGame(action: UIAlertAction! = nil) {
+        score = 0
+    }
+    
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+       
+        scoreLable.textColor = .black
+        scoreLable.font = UIFont.boldSystemFont(ofSize: 24)
+        scoreLable.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scoreLable)
+        
+        NSLayoutConstraint.activate([
+            
+            scoreLable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -20),
+            scoreLable.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
-        
+        let finishScore = UIAlertController(title: "You win", message: "You have maximum points", preferredStyle: .alert)
+        finishScore.addAction(UIAlertAction(title: "Restart the game", style: .default, handler: finishGame))
+        let notCorrect = countries[sender.tag]
+       
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
         } else {
-            title = "Wrong"
+            title = "Wrong is \(notCorrect)"
             score -= 1
         }
+        
+    if score == 10 {
+        present(finishScore,animated: true)
+    }
         
         let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
         
         ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         
         present(ac, animated: true)
+        
+        }
     }
-}
