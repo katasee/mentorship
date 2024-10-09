e2575fa2b04cb06b5e93cafe27a3c746d8e7b224
katasee <kotalov.developer@gmail.com>
2024-10-09 19:33:54 +0200

    My project1

--- HackingWithSwift/SwiftUI/Project1/MyProject/MyProject/ContentView.swift
+++ HackingWithSwift/SwiftUI/Project1/MyProject/MyProject/ContentView.swift
@@ -8,40 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var usd = 0.0
-    @State private var pln = 0.0
-    @FocusState private var focus: Bool
+    @State private var currencyToExchange = "USD"
+    @State private var currencyExchanged = "EUR"
+    @State private var amount: Double = 0
+    var currency = ["USD", "EUR", "PLN"]
     
-    var exchange: Double{
-        let afterChange = usd * 3.9
-        return afterChange
+    let usd = ["USD": 1.1, "EUR": 1.0, "PLN": 4.3]
+    let eur = ["USD": 1.0, "EUR": 0.91, "PLN": 3.9]
+    let pln = ["USD": 0.26, "EUR": 0.23, "PLN": 1.0]
+    
+    func convert () {
+        if currencyToExchange == "USD" && currencyExchanged == "EUR" {
+            guard let rate = usd["EUR"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "USD" && currencyExchanged == "PLN" {
+            guard let rate = usd["PLN"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "USD" && currencyExchanged == "USD" {
+            guard let rate = usd["USD"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "EUR" && currencyExchanged == "EUR" {
+            guard let rate = eur["EUR"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "EUR" && currencyExchanged == "PLN" {
+            guard let rate = eur["PLN"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "EUR" && currencyExchanged == "USD" {
+            guard let rate = eur["USD"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "PLN" && currencyExchanged == "PLN" {
+            guard let rate = pln["PLN"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "PLN" && currencyExchanged == "USD" {
+            guard let rate = pln["USD"] else { return }
+            amount = rate * amount
+        } else if currencyToExchange == "PLN" && currencyExchanged == "EUR" {
+            guard let rate = pln["EUR"] else { return }
+            amount = rate * amount
+            return
+        }
     }
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Write your $ value"){
-                    TextField("Amount", value: $usd, format: .currency(code: "USD"))
-                        .keyboardType(.decimalPad)
-                        .focused($focus)
-                                 }
-                
-                Section ("Your PLN value") {
-                    Text(exchange, format: .currency(code: "PLN"))
-                }
-            }
-            .navigationTitle("Exchange")
-            .toolbar {
-                if focus {
-                    Button ("Done") {
-                        focus = false
+        VStack {
+            NavigationStack{
+                Form {
+                    Section ("Curreency convert") {
+                        TextField("Enter amout", value: $amount, format: .number)
+                            .keyboardType(.decimalPad)
                     }
+                    Section {
+                        Picker("FROM", selection: $currencyToExchange) {
+                            ForEach (currency, id:\.self) {
+                                Text($0)
+                                    .onChange(of: currencyToExchange) { newValue in
+                                        convert()
+                                    }
+                            }
+                        }
+                        Picker("TO", selection: $currencyExchanged) {
+                            ForEach (currency, id:\.self) {
+                                Text($0)
+                                    .onChange(of: currencyExchanged) { newValue in
+                                        convert()
+                                    }
+                            }
+                        }
+                        }
+                    Section ("Conversion") {
+                        Text(amount, format: .number)
+                            
+                            
+                    }
+                    .pickerStyle(.menu)
                     
                 }
+                .navigationTitle("Exchange24")
             }
         }
     }
 }
+
+
+
+
 #Preview {
     ContentView()
 }
