e30d60d2a813cb6f8aa4db7f26414005dea94d1e
katasee <kotalov.developer@gmail.com>
2024-10-10 19:15:55 +0200

    First project update

--- HackingWithSwift/SwiftUI/Project1/MyProject/MyProject/ContentView.swift
+++ HackingWithSwift/SwiftUI/Project1/MyProject/MyProject/ContentView.swift
@@ -15,8 +15,8 @@ struct ContentView: View {
     @State private var convertToAmount: Double = 0
     var currency = ["USD", "EUR", "PLN"]
     
-    let usd = ["USD": 1.1, "EUR": 1.0, "PLN": 4.3]
-    let eur = ["USD": 1.0, "EUR": 0.91, "PLN": 3.9]
+    let usd = ["USD": 1.0, "EUR": 1.1, "PLN": 4.3]
+    let eur = ["USD": 0.91, "EUR": 1.0, "PLN": 3.9]
     let pln = ["USD": 0.26, "EUR": 0.23, "PLN": 1.0]
     
     func convert () {
@@ -51,50 +51,94 @@ struct ContentView: View {
         }
     }
     
+    let background = Color(UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0))
     var body: some View {
-        VStack {
-            NavigationStack{
-                Form {
-                    Section ("Curreency convert") {
-                        TextField("Enter amout", value: $amount, format: .currency(code: currencyToExchange))
-                            .keyboardType(.decimalPad)
-                    }
-                    Section {
-                        Picker("FROM", selection: $currencyToExchange) {
-                            ForEach (currency, id:\.self) {
-                                Text($0)
-                                    .onChange(of: currencyToExchange) { newValue in
-                                        convert()
-                                    }
+        
+        NavigationStack {
+            ZStack{
+                background
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack{
+                        VStack {
+                            Spacer(minLength: 32)
+//                            Section ("Curreency convert") {
+                                Text("Curreency convert")
+                                .font(.subheadline)
+                                .foregroundStyle(Color.gray)
+                                .frame(width: 350, height: 15,alignment: .leading)
+
+                                TextField("Enter amout", value: $amount, format: .currency(code: currencyToExchange))
+                                    .keyboardType(.decimalPad)
+                                    .padding(11)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(Color.white))
+                            
                             }
                         }
-                        Picker("TO", selection: $currencyExchanged) {
-                            ForEach (currency, id:\.self) {
-                                Text($0)
-                                    .onChange (of: currencyExchanged) { newValue in
-                                        convert()
-                                    }
-                            }
-                        }
-                    }
-                    Section ("Conversion") {
-                        Text(convertToAmount, format: .currency(code: (currencyExchanged)))
-                        
-                        
-                    }
-                    .pickerStyle(.menu)
                     
+                    VStack (spacing: 1) {
+                            Picker("FROM", selection: $currencyToExchange) {
+                                ForEach (currency, id:\.self) {
+                                    Text($0)
+                                        .onChange(of: currencyToExchange) { newValue in
+                                            convert()
+                                        }
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(7)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.white))
+                            
+                        Divider()
+                        
+                            Picker("TO", selection: $currencyExchanged) {
+                                ForEach (currency, id:\.self) {
+                                    Text($0)
+                                        .onChange (of: currencyExchanged) { newValue in
+                                            convert()
+                                            
+                                        }
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(7)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.white))
+                            
+                        
+                        
+                    }
+                        VStack {
+                            Text("Conversion")
+                            .font(.subheadline)
+                            .foregroundStyle(Color.gray)
+                            .frame(width: 350, height: 15,alignment: .leading)
+                            Text(convertToAmount, format: .currency(code: (currencyExchanged)))
+                                .frame(width: 350, height: 30,alignment: .leading)
+                                .padding(7)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.white))
+                            
+                                
+                                
+                        }
+//                        .pickerStyle(.menu)
+                    }
+             .padding(.horizontal, 24)
                 }
-                .navigationTitle("Exchange24")
+                .navigationTitle("Exchange 24")
+                
             }
-        }
-        .onChange (of: amount) { newValue in
-            convert()
+            
         }
     }
-    
-    
-}
+
 
 #Preview {
     ContentView()
