5edd6fd3c5784706c77e23e172bb3c72988e1044
katasee <kotalov.developer@gmail.com>
2024-10-14 18:50:21 +0200

    Project 2 SwiftUI

---
+++ HackingWithSwift/SwiftUI/Project2/Guess the Flag/Guess the Flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Katasee on 12/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
