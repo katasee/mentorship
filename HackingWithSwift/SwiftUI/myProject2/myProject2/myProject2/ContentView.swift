9cb40eab8658427ca6a99c6d9bd67f5220b33b54
katasee <kotalov.developer@gmail.com>
2024-10-20 21:03:08 +0200

    MyProject 2

--- HackingWithSwift/SwiftUI/myProject2/myProject2/myProject2/ContentView.swift
+++ HackingWithSwift/SwiftUI/myProject2/myProject2/myProject2/ContentView.swift
@@ -6,84 +6,193 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var computerСhoice: Int = Int.random(in: 0...2)
+    @State private var rightСhoice: Bool = false
+    @State private var optionToСhoice: [String] = ["✊", "✋", "✌️"].shuffled()
+    @State private var score: Int = 0
+    @State private var myChoise: String = ""
+    @State private var counter: Int = 0
+    @State private var endGame: Bool = false
+    @State private var finalScore: String = ""
+    
     var body: some View {
         NavigationStack{
             ZStack{
                 LinearGradient(colors:[.blue, .yellow], startPoint: .top, endPoint: .bottom)
                     .ignoresSafeArea()
-               
                 
                 VStack(spacing: 50) {
                     Spacer()
                     VStack  {
-                        Text("Win/lose")
+                        compChoose()
+                        goodChoise()
                     }
+                    .bold()
+                    .font(.system(size: 20))
                     
                     VStack{
-                        
-                        Image("rock")
-                            
+                        myturn()
                     }
-                    .padding(30)
-                    .background(Color.secondary)
-                    .clipShape(.circle)
-                    .shadow(radius: 20)
-                    
-                    Spacer()
-                    
                     
                     HStack (spacing: 15) {
                         HStack {
-                            Button("Rock ✊") {
-                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            Button("✊"){
+                                counter += 1
+                                rock()
                             }
-                        }
-                        .padding(20)
-                        .background(.black)
-                        .foregroundStyle(.white)
-                        .clipShape(.buttonBorder)
-                        
-                        
-                        
-                        HStack {
-                            Button("Peper ✋") {
-                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            .padding(20)
+                            .background(Color.black)
+                            .clipShape(.buttonBorder)
+                            .font(.system(size: 70))
+                            
+                            Button("✋") {
+                                counter += 1
+                                paper()
                             }
-                        }
-                        .padding(20)
-                        .background(.black)
-                        .foregroundStyle(.white)
-                        .clipShape(.buttonBorder)
-                        
-                        HStack{
-                            Button("Scissons ✌️") {
-                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                            .padding(20)
+                            .background(.black)
+                            .clipShape(.buttonBorder)
+                            .font(.system(size: 70))
+                            
+                            Button("✌️"){
+                                counter += 1
+                                scissons()
                             }
+                            .padding(20)
+                            .background(.black)
+                            .clipShape(.buttonBorder)
+                            .font(.system(size: 70))
                         }
-                        .padding(20)
-                        .background(.black)
-                        .foregroundStyle(.white)
-                        .clipShape(.buttonBorder)
                     }
+                    
                     VStack{
                         Spacer()
-                        VStack  {
-                            Text("Score:")
-                            
-                        }
-                        Spacer()
+                        Text("Score: \(score)")
                     }
+                    .font(.system(size: 40))
                 }
-                
+            }
+            .alert("Game over", isPresented: $endGame) {
+                Button ("Restart") {}
+            } message: {
+                Text("\(finalScore)")
             }
             .navigationTitle("Rock, Paper, Scissons!")
         }
+    }
+    
+    func compChoose() -> some View {
+        Text(optionToСhoice[computerСhoice])
+            .font(.largeTitle.weight(.semibold))
+    }
+    
+    func rock()  {
+        if computerСhoice == 0 {
+            myChoise = "✊"
+            score += 1
+            newGame()
+            rightСhoice = true
+        }
+        else if computerСhoice == 1 {
+            myChoise = "✊"
+            score -= 1
+            newGame()
+        } else {
+            myChoise = "✊"
+            score += 1
+            newGame()
+            rightСhoice = true
+        }
+    }
+    
+    func paper() {
+        if computerСhoice == 0 {
+            myChoise = "✋"
+            score += 1
+            newGame()
+            rightСhoice = true
+        }
+        else if computerСhoice == 1 {
+            myChoise = "✋"
+            score += 1
+            newGame()
+            rightСhoice = true
+        } else {
+            myChoise = "✋"
+            score -= 1
+            newGame()
+        }
+    }
+    
+    func scissons() {
+        if computerСhoice == 0 {
+            myChoise = "✌️"
+            score -= 1
+            newGame()
+        }
+        else if computerСhoice == 1 {
+            myChoise = "✌️"
+            score += 1
+            newGame()
+            rightСhoice = true
+        } else {
+            myChoise = "✌️"
+            score += 1
+            newGame()
+            rightСhoice = true
+        }
+    }
+    
+    func myturn() -> some View {
+        Text("\(myChoise)")
+            .font(.system(size: 150))
+            .padding(25)
+            .background(Color.secondary)
+            .clipShape(.circle)
+            .shadow(radius: 40)
+    }
+    
+    func goodChoise() -> some View {
+        if rightСhoice == true {
+            Text("You win")
+        } else {
+            Text("You lose")
+        }
+    }
+    
+    func newGame () {
+        computerСhoice = Int.random(in: 0...2)
+        rightСhoice = false
+        if counter == 10 {
+            restart()
+        }
         
+        func restart() {
+            endGame = true
+            finalScore = "Your score is \(score)"
+            resetGame()
+        }
+        
+        func resetGame () {
+            score = 0
+            counter = 0
+        }
     }
 }
-
+//        func button() -> some View {
+//            ForEach(0..<3) { number in
+//                Button {
+//                } label: {
+//                    Text(optionToСhoice[number])
+//        .padding(20)
+//        .background(.black)
+//        .foregroundStyle(.white)
+//        .clipShape(.buttonBorder)
+//        .font(.system(size: 70))
+//                }
+//            }
+//        }
 
 
 
