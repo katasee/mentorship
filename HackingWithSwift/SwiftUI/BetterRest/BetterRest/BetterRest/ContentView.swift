4f71d62ef8828f43f3b4c3c767a47c104f9821d1
katasee <kotalov.developer@gmail.com>
2024-10-26 19:16:17 +0200

    Update project

--- HackingWithSwift/SwiftUI/BetterRest/BetterRest/BetterRest/ContentView.swift
+++ HackingWithSwift/SwiftUI/BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -9,14 +9,13 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = defaultWakeTime
-    @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
-    
+    @State private var wakeUp: Date = defaultWakeTime
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
     @State private var alertTitle: String = ""
     @State private var alertMessage: String = ""
-    @State private var showingAlert = false
-    @State private var sleepTime = ""
+    @State private var showingAlert: Bool = false
+    @State private var sleepTime: String = ""
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -25,44 +24,61 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? .now
     }
 
-    
     var body: some View {
         NavigationStack {
             Form {
-                Section ("When do you want to wake up?") {
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        
-                }
-                
-                Section("Describe amount of sleep") {
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                
-                Section ("Daily coffee intake") {
-                    Picker ("^[Please enter a cup](inflect: true)", selection: $coffeeAmount) {
-                        ForEach(0..<20) {
-                            Text($0,format: .number)
-                        }
-                    }
-                }
-                Section("Calculate") {
-                    Text(sleepTime)
-                }
-                
+                datePicker
+                sleepAmountStepper
+                coffeeAmountPicker
+                calculate
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-               
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {}
-            } message: {
-                Text(alertMessage)
+        }
+    }
+    
+    var datePicker: some View{
+        Section ("When do you want to wake up?") {
+            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+        }
+        .onChange(of: wakeUp) {_ in
+            calculateBedTime()
+        }
+    }
+    
+    var sleepAmountStepper: some View {
+        Section("Describe amount of sleep") {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        }
+        .onChange(of: sleepAmount) {_ in
+            calculateBedTime()
+        }
+    }
+    
+    var coffeeAmountPicker: some View {
+        Section ("Daily coffee intake") {
+            Picker ("^[Please enter a cup](inflect: true)", selection: $coffeeAmount) {
+                ForEach(0..<20) {
+                    Text($0,format: .number)
+                }
             }
         }
-    
+        .onChange(of: coffeeAmount) {_ in
+            calculateBedTime()
+        }
     }
-    func calculateBedTime () {
+    
+    var calculate: some View {
+        Section("Calculate") {
+            HStack {
+                 Text("Your ideal bedtime is")
+                 Spacer()
+                 Text("\(sleepTime)")
+             }
+            .font(.title2)
+         }
+    }
+     
+ func calculateBedTime () {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
